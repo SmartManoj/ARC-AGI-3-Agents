9487ace0cd5bb9ea1ada2651f344526d28630df6
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-07-24 16:43:55 +0530

    Refactor and extend ARC API playground script
    Introduced a requests.Session for persistent connections, added functions to open a scorecard and perform a reset action, and updated the main block to demonstrate a full workflow: open, reset, get, close scorecard, and open the scorecard URL in a browser. Improved headers and refactored API calls to use the session object.

--- scripts/api_playground.py
+++ scripts/api_playground.py
@@ -5,12 +5,24 @@ load_dotenv()
 
 ARC_API_KEY = os.getenv('ARC_API_KEY') 
 
+s = requests.Session()
+
 headers = {
     'x-api-key': ARC_API_KEY,
+    'Accept': 'application/json',
 }
 
+def open_scorecard():
+    response = s.post(
+        f'https://three.arcprize.org/api/scorecard/open',
+        headers=headers,
+        json={}
+    )
+    print(response.json())
+    return response.json()['card_id']
+
 def close_scorecard(card_id):
-    response = requests.post(
+    response = s.post(
         f'https://three.arcprize.org/api/scorecard/close',
         json={'card_id': card_id},
         headers=headers,
@@ -18,13 +30,41 @@ def close_scorecard(card_id):
     print(response.json())
 
 def get_scorecard(card_id):
-    response = requests.get(
+    response = s.get(
         f'https://three.arcprize.org/api/scorecard/{card_id}',
         headers=headers,
     )
     print(response.json())
 
+def do_reset_action(card_id):
+    data = {
+        "game_id": "ft09-16726c5b26ff",
+        "guid": None,
+        "card_id": card_id,
+    }
+    response = s.post(
+        f'https://three.arcprize.org/api/cmd/RESET',
+        headers=headers,
+        json=data
+    )
+    guid = (response.json())['guid']
+    print(response.ok)
+    return guid
+
+
 if __name__ == "__main__":
-    card_id = '7ac1181c-d87f-45e4-a701-57483c20d53c'
-    close_scorecard(card_id)
+    card_id = '361d01b9-ac37-495c-b361-bd4a58713d82'
+    print('Opening scorecard')
+    card_id = open_scorecard()
+    print('Resetting game')
+    do_reset_action(card_id)
+    print('Getting scorecard')
     get_scorecard(card_id)
+    print('Closing scorecard')
+    close_scorecard(card_id)
+    print('Opening URL')
+    # open URL
+    import webbrowser
+    scorecard_url = f'https://three.arcprize.org/scorecards/{card_id}'
+    print(scorecard_url)
+    webbrowser.open(scorecard_url)
