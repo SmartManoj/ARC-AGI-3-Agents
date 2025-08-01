693cdd728c8414e7d0e06ea42176ec2c2a111f94
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-08-01 15:18:42 +0530

    l3

--- games/vc33.py
+++ games/vc33.py
@@ -46,35 +46,41 @@ objs = detect_objects(grid,  single_color_only=0)
 main_obj = [obj for obj in objs if obj.height != 1][0]
 objs = detect_objects((main_obj), single_color_only=1)
 print('number of objs',len(objs))
-red_handle = [obj for obj in objs if obj.color == FrameColor.RED.value][0]
-blue_handle = [obj for obj in objs if obj.color == FrameColor.BLUE.value][0]
+red_handles = [obj for obj in objs if obj.color == FrameColor.RED.value]
+blue_handles = [obj for obj in objs if obj.color == FrameColor.BLUE.value]
 for obj in objs:
-    if obj.color not in [FrameColor.BLUE.value, FrameColor.RED.value] and obj.height == 4 and obj.width == 4:
-        indicator_obj = obj
-        break
+    if obj.color not in [FrameColor.BLUE.value, FrameColor.RED.value]:
+        if obj.height == 4 and obj.width == 4:
+            indicator_obj = obj
+        if obj.height == 8 and obj.width == 8:
+            floating_obj = obj
 
 water_bodies = [obj for obj in objs if obj.color == FrameColor.WHITE.value]
-objs = [red_handle, blue_handle, indicator_obj, *water_bodies, main_obj]
 # plot_grids(objs)
-print('red handle', red_handle.region.x1, red_handle.region.y1)
-print('blue handle', blue_handle.region.x1, blue_handle.region.y1)
+for red_handle in red_handles:
+    print('red handle', red_handle.region.x1, red_handle.region.y1)
+for blue_handle in blue_handles:
+    print('blue handle', blue_handle.region.x1, blue_handle.region.y1)
 print('indicator obj', indicator_obj.region.x1, indicator_obj.region.y1)
-for wb in water_bodies:
-    print(wb.region.x1, wb.region.x2, wb.region.y1, wb.region.y2)
-    if wb.region.x2 == blue_handle.region.x2:
-        blue_water_body = wb
-    elif wb.region.x1 == red_handle.region.x1:
-        red_water_body = wb
-print(red_water_body.region.y1, indicator_obj.region.y1)
-times = (indicator_obj.region.y1 - red_water_body.region.y1 )//4
+print('floating obj', floating_obj.region.x1, floating_obj.region.y1)
+for wb_idx,wb in enumerate(water_bodies):
+    # find target water body
+    if wb.region.x1 < floating_obj.region.x1 < wb.region.x2:
+        target_water_body = wb
+        target_water_body_idx = wb_idx
+        break
+
+times = (indicator_obj.region.y1 - target_water_body.region.y1 )//4
 print(times)
 if times > 0:
-    handle = blue_handle
+    handle = blue_handles[target_water_body_idx-1]
 else:
-    handle = red_handle
-for _ in range(abs(times)):
-    x, y = handle.region.x1, handle.region.y1
-    execute_action('ACTION6', x=x, y=y)
+    handle = red_handles[target_water_body_idx-1]
+from pymsgbox import confirm
+if confirm('Continue?') == 'OK':
+    for _ in range(abs(times)):
+        x, y = handle.region.x1, handle.region.y1
+        execute_action('ACTION6', x=x, y=y)
 
 
 
