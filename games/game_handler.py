68ee0a5ee8056f5af1afa23e1370e79826854160
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-08-01 12:16:14 +0530

    Refactor execute_action to remove object_number param
    Removed the unused 'object_number' parameter from the execute_action function and updated the print statement and request parameters accordingly. Introduced a PORT constant for easier port configuration.

--- games/game_handler.py
+++ games/game_handler.py
@@ -1,13 +1,15 @@
 import requests
 
-def execute_action(action: str, x: int = None, y: int = None, object_number: int = None) -> dict:
-    print(f"Executing action: {action}, x: {x}, y: {y}, object_number: {object_number}")
-    url = "http://localhost:8000/execute_action"
+PORT= 8000
+
+def execute_action(action: str, x: int = None, y: int = None) -> dict:
+    print(f"{PORT} | Executing action: {action}, x: {x}, y: {y}")
+    url = f"http://localhost:{PORT}/execute_action"
     response = requests.get(url, params={
         "action": action,
         "x": x,
         "y": y,
-        "object_number": object_number})
+        })
     return response.json()
 
 if __name__ == '__main__':
